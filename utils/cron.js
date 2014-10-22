be2f93762c14b2deb9e6c44e740286573c91a8e0
Arsey <arseysensector@gmail.com>
2014-10-22 22:44:05 +0300

    delete zip's older than 1 day

--- utils/cron.js
+++ utils/cron.js
@@ -3,6 +3,15 @@ var _ = require('underscore');
 var walk = require('walk');
 var fs = require('fs');
 
+function removeOneDayOldFile(root, stat, next) {
+    var outOfDate = (new Date().getTime() - stat.mtime.getTime()) / 1000 >= (24 * 60 * 60);
+    if (outOfDate) {
+        fs.unlink(root + '/' + stat.name, function () {
+            next();
+        })
+    }
+}
+
 module.exports = function (callback) {
     var cronJob = require('cron').CronJob;
 
@@ -11,17 +20,18 @@ module.exports = function (callback) {
         winston.info('--- CRON HEARTBEAT ---');
 
         // Walker options
-        var walker = walk.walk('./uploads', { followLinks: false });
-
+        var imgsWalker = walk.walk('./uploads', { followLinks: false });
         //auto delete images older than 1 day
-        walker.on('file', function (root, stat, next) {
-            // auto delete images older than 1 day
-            var outOfDate = (new Date().getTime() - stat.mtime.getTime()) / 1000 >= (24 * 60 * 60);
-            if (outOfDate) {
-                fs.unlink(root + '/' + stat.name, function () {
-                    next();
-                })
-            }
+        imgsWalker.on('file', function (root, stat, next) {
+            removeOneDayOldFile(root, stat, next)
+        });
+
+
+        // Walker options
+        var zipWalker = walk.walk('./zip', { followLinks: false });
+        //delete zip's older than 1 day
+        zipWalker.on('file', function (root, stat, next) {
+            removeOneDayOldFile(root, stat, next)
         });
     }).start();
 
